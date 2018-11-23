f31ecfc4405daa6894066b8d0c03045fc767119e
evanhearne <evanhearne02@gmail.com>
2018-11-23 15:33:24 +0000

    added calculator functionality besides deletion

--- calculatorproject.js
+++ calculatorproject.js
@@ -2,5 +2,17 @@ function addNumberToDisplay(number) {
 var displayScreen = document.getElementById('displayScreen');
 var displayScreenValue = displayScreen.value;
 displayScreen.value = displayScreenValue + number;
+}
 
+function addOperatorToDisplay(operator) {
+	var displayScreen = document.getElementById('displayScreen');
+	var displayScreenValue = displayScreen.value;
+	displayScreen.value = displayScreenValue + operator;
+}
+
+function evaluateEquation() {
+	var displayScreen = document.getElementById('displayScreen');
+	var displayScreenValue = displayScreen.value;
+	var evaluateEquation = eval(displayScreen.value)
+    displayScreen.value = evaluateEquation;
 }
